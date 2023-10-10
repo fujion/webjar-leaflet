55b1e454ac47e8a26c90c6f43d6a6c5efdfec052
Douglas Martin <mdgeek1@gmail.com>
2023-10-10 17:44:24 -0400

    Use new webjar validator plugin.

--- validate_.sh
+++
@@ -1,7 +0,0 @@
-file_exists "import-map.json"
-file_exists "README.md"
-file_contains "LICENSE" "BSD 2-Clause License"
-
-file_exists "dist/leaflet.js"
-file_exists "dist/leaflet.css"
-dir_exists "dist/images"
